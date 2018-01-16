4fd2f83a8a7f8f1ace9f136ebc401d26711365ab
Oron Nadiv <oron@nadiv.us>
2018-01-15 23:51:47 -0800

    :thumbsup: Add support for facebookincubator/create-react-app environment variables.

--- src/config.js
+++ src/config.js
@@ -2,29 +2,35 @@ import DebugLib from 'debug'
 
 const error = DebugLib('ha:pubnub:config:error')
 
-export const publishKey = process.env.PUBNUB_PUBLISH_KEY
+export const publishKey = process.env.PUBNUB_PUBLISH_KEY || process.env.REACT_APP_PUBNUB_PUBLISH_KEY
 if (!publishKey) {
-  error('PubNub publish key could not be found in the environment variable.  Please set \'PUBNUB_PUBLISH_KEY\'.')
-  if (process) {
+  const message = 'PubNub publish key could not be found in the environment variable.  Please set \'PUBNUB_PUBLISH_KEY\' or \'REACT_APP_PUBNUB_PUBLISH_KEY\'.'
+  error(message)
+  if (process && process.exit) {
     process.exit(100)
   }
+  console.error(message)
 }
 
-export const subscribeKey = process.env.PUBNUB_SUBSCRIBE_KEY
+export const subscribeKey = process.env.PUBNUB_SUBSCRIBE_KEY || process.env.REACT_APP_PUBNUB_SUBSCRIBE_KEY
 if (!subscribeKey) {
-  error('PubNub subscribe key could not be found in the environment variable.  Please set \'PUBNUB_SUBSCRIBE_KEY\'.')
-  if (process) {
+  const message = 'PubNub subscribe key could not be found in the environment variable.  Please set \'PUBNUB_SUBSCRIBE_KEY\' or \'REACT_APP_PUBNUB_SUBSCRIBE_KEY\'.'
+  error(message)
+  if (process && process.exit) {
     process.exit(200)
   }
+  console.error(message)
 }
 
 export const secretKey = () => {
   const res = process.env.PUBNUB_SECRET_KEY
   if (!res) {
-    error('PubNub secret key could not be found in the environment variable.  Please set \'PUBNUB_SECRET_KEY\'.')
-    if (process) {
+    const message = 'PubNub secret key could not be found in the environment variable.  Please set \'PUBNUB_SECRET_KEY\'.'
+    error(message)
+    if (process && process.exit) {
       process.exit(300)
     }
+    console.error(message)
   }
   return res
 }
